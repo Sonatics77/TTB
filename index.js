712e1b571599f14558105fbc2c531343b53328fb
Sonatics77 <thenuga.dealwis@gmail.com>
2018-10-30 17:44:04 +1100

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const Botconfig = require("/botconfig.json");
+const Botconfig = require("botconfig.json");
 const Discord = require("discord.js");
 const Fs = require("fs");
 const token = process.env.token;
